ea6e3bd0ea40032991d38d03d033e8053ed44b74
Naser Aleisa <aleisa.naser@gmail.com>
2023-12-26 01:20:02 +0300

    use @antfu/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    stylistic: true,
+    svelte: true,
+    typescript: { tsconfigPath: 'tsconfig.lint.json' },
+  },
+  {
+    name: 'my-additional-svelte-rules',
+    files: ['**/*.svelte'],
+    rules: {
+      'svelte/first-attribute-linebreak': 'error',
+      'svelte/max-attributes-per-line': 'error',
+      // ...
+      // more rules: https://sveltejs.github.io/eslint-plugin-svelte/rules/
+    },
+  },
+)
