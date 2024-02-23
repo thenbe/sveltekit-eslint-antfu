b7f4d46177ebf470c2dc0158047ff4074a2265cd
Naser Aleisa <aleisa.naser@gmail.com>
2024-02-23 20:44:20 +0300

    minimal repro for type aware rules on svelte files

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,11 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
-    stylistic: true,
+    stylistic: false,
     svelte: true,
     typescript: {
       tsconfigPath: 'tsconfig.lint.json',
       filesTypeAware: [`**\/*.{ts,tsx,svelte}`], // Enable type-aware rules in svelte files (e.g. `no-floating-promises`).
-    },
-  },
-  {
-    name: 'my-additional-svelte-rules',
-    files: ['**/*.svelte'],
-    rules: {
-      'svelte/first-attribute-linebreak': 'error',
-      'svelte/max-attributes-per-line': 'error',
-      // ...
-      // more rules: https://sveltejs.github.io/eslint-plugin-svelte/rules/
     },
   },
 )
